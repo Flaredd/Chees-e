7af7e6e28a0e71d229cd75e0f42802c6bc53aa2f
2021_ <>
2021-04-29 09:00:10 +0000

    no more ugly img

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ async def on_ready():
     if message.content.startswith('chees e'):
       await message.channel.send('https://cdn.discordapp.com/attachments/837072531420479530/837244529282646016/Cheese_meme.gif')
       time.sleep(3.5)
-      await message.channel.send('https://cdn.discordapp.com/attachments/837072531420479530/837210836656979988/Ua9ddbc0277b84e37b4395c2dfe5eb40aV.jpg ')
-      time.sleep(0.5)
-      await message.channel.send('chees e')
+#      await message.channel.send('https://cdn.discordapp.com/attachments/837072531420479530/837210836656979988/Ua9ddbc0277b84e37b4395c2dfe5eb40aV.jpg ')
+#      time.sleep(0.5)
+      await message.channel.send(':cheese: chees e')
 
 client.run(os.getenv('TOKEN'))    
